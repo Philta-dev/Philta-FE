1d0bb0bec2bd4b0849b7696b0b1aabd30bc17e4d
0xseo <0xseo94@gmail.com>
2024-05-24 16:49:03 +0900

    FEAT/ 가로 scrollview (flatlist)

--- src/pages/Indexing.tsx
+++ src/pages/Indexing.tsx
@@ -1,9 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {useState} from 'react';
+import {
+  FlatList,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function Indexing() {
+  const [bookname, setBookname] = useState(-1);
+  const [testament, setTestament] = useState('old');
   return (
     <View>
       <Text>Indexing</Text>
+      <Pressable
+        onPress={() => {
+          if (testament == 'old') setTestament('new');
+          else setTestament('old');
+        }}>
+        <Text style={{color: 'black'}}>{testament}</Text>
+      </Pressable>
+      <View style={{backgroundColor: 'red', height: 100, width: '100%'}}>
+        <FlatList
+          horizontal={true}
+          data={
+            testament == 'old'
+              ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+              : [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+          }
+          renderItem={({item, index}) => (
+            <Pressable
+              style={{padding: 10, margin: 10, backgroundColor: 'white'}}
+              onPress={() => setBookname(item)}>
+              <Text style={{color: 'black'}}>{item}</Text>
+            </Pressable>
+          )}
+        />
+      </View>
+      <Text style={{color: 'black'}}>{bookname}</Text>
     </View>
   );
 }
