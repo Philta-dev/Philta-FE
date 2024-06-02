2551c2c51d2c021bfda68cc85ce32ccf40dd401f
0xseo <0xseo94@gmail.com>
2024-06-03 00:36:06 +0900

    FEAT/ kakao login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import {Pressable, StyleSheet, View} from 'react-native';
 import Config from 'react-native-config';
 import {SignInNavParamList} from '../../AppInner';
 import Text from '../components/Text';
+import * as KakaoLogin from '@react-native-seoul/kakao-login';
 
 type SignInPageNavigationProp = NativeStackNavigationProp<
   SignInNavParamList,
@@ -15,10 +16,22 @@ type SignInProps = {
 
 export default function SignIn(props: SignInProps) {
   const navigation = props.navigation;
+
+  const LoginWithKakao = async () => {
+    console.log('카카오 로그인');
+    const token = await KakaoLogin.login();
+    const profile = await KakaoLogin.getProfile();
+    try {
+      console.log(token);
+      console.log(profile.nickname);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View>
-      <Text>{Config.API_URL}</Text>
-      <Pressable style={styles.kakao}>
+      {/* <Text>{Config.API_URL}</Text> */}
+      <Pressable style={styles.kakao} onPress={() => LoginWithKakao()}>
         <Text>카카오</Text>
       </Pressable>
       <Pressable style={styles.apple}>
