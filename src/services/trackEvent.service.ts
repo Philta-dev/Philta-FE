e11b85bba374acb997e1a8176b237e0b76856032
0xseo <0xseo@gmail.com>
2024-07-11 09:56:01 +0900

    FEAT/ get user's languages

--- src/services/trackEvent.service.ts
+++ src/services/trackEvent.service.ts
@@ -7,13 +7,20 @@ const mixpanel = new Mixpanel(
   trackAutomaticEvents,
 );
 
+import {getLocales} from 'react-native-localize';
+
+const getCurrentDeviceLanguage = () => {
+  const locales = getLocales();
+  return locales[0].languageCode; // id
+};
+
 export function setTrackUser() {
   mixpanel.init();
   mixpanel.setLoggingEnabled(true);
 
   mixpanel.identify('USER_ID');
 
-  mixpanel.getPeople().set('$name', 'Jane Doe');
+  mixpanel.getPeople().set('language', getCurrentDeviceLanguage());
   mixpanel.getPeople().set('$email', 'jane.doe@example.com');
   mixpanel.getPeople().set('plan', 'Premium');
 
